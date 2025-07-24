b2d3cca58445581b6b4bb40a742bbb4e5c4e71b2
andy ramaroson <andyramaroson@gmail.com>
2025-07-24 22:35:25 +0200

    [Feature] create database.js to connect mongoDb add settings.json and update .gitignore

---
+++ Back-end/src/config/database.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const connectDB = async () => {
+  try {
+    const mongoURI =
+      process.env.NODE_ENV === 'test'
+        ? process.env.MONGODB_TEST_URI
+        : process.env.MONGODB_URI
+
+    const conn = await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+
+      useUnifiedTopology: true,
+    })
+
+    console.log(`MongoDB connecté: ${conn.connection.host}`)
+  } catch (error) {
+    console.error('Erreur de connexion à MongoDB:', error.message)
+
+    process.exit(1)
+  }
+}
+
+module.exports = connectDB
