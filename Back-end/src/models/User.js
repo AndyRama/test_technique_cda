543d2d8cafacb46d4a9564a0dbfdf7e23d1b32cd
andy ramaroson <andyramaroson@gmail.com>
2025-07-24 22:53:07 +0200

    [Feature and fix] create file errorHandler, create file model User and fix error converted to an ES module

--- Back-end/src/models/User.js
+++ Back-end/src/models/User.js
@@ -2,7 +2,7 @@ import { Schema } from 'mongoose';
 
 // schema model User
 // Index pour les recherches
-// Middleware pour hasher le mot de passe avant sauvegarde
+// Middleware pour hasher le mot de passe avant sauvegarde(bycrypt)
 // Méthode pour comparer les mots de passe
 // Méthode pour exclure le mot de passe des réponses JSON
 // Virtual pour l'ID sans underscore
