be5db2fa7423cd7b6c462c8fa8dd1d1871366a19
andy ramaroson <andyramaroson@gmail.com>
2025-07-24 22:03:11 +0200

    [feature] add server and update .env

---
+++ Back-end/server.js
@@ -0,0 +1,14 @@
+const app = require('./src/app');
+const connectDB = require('./src/config/database');
+
+const PORT = process.env.PORT || 5000;
+
+// Connexion à la base de données
+
+connectDB();
+
+const server = app.listen(PORT, () => {
+  console.log(`Serveur démarré sur le port ${PORT}`);
+});
+
+module.exports = server;
